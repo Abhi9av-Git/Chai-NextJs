0b4927f65041a97dd4e28ad4ea1469c0567c2dbf
abhinav <abhinav31102004@gmail.com>
2025-07-14 15:33:16 +0530

    Added Navbar and Message Card

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -27,15 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <Navbar />
           {children}
-          <Toaster />
-        </body>
-      </AuthProvider> 
+        </body> 
     </html>
   );
 }
